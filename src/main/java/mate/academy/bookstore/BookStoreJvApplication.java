b5716b76ae8aa55359144036e51dfb8f65256684
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-12-02 12:03:43 +0200

    done hw

--- src/main/java/mate/academy/bookstore/BookStoreJvApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreJvApplication.java
@@ -1,13 +1,37 @@
 package mate.academy.bookstore;
 
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BookStoreJvApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreJvApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setTitle("Code jv");
+                book.setPrice(BigDecimal.valueOf(1000));
+
+                bookService.save(book);
+
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
 }
---
+++ src/main/java/mate/academy/bookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,55 @@
+package mate.academy.bookstore.repository;
+
+import mate.academy.bookstore.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(book);
+            transaction.commit();
+
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new RuntimeException(e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Book", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
