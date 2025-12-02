d1553e36384abcdaf64c8e5df8d14ce7069e4258
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-12-02 11:01:05 +0200

    init

---
+++ src/main/java/mate/academy/bookstore/BookStoreJvApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreJvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreJvApplication.class, args);
+    }
+
+}
