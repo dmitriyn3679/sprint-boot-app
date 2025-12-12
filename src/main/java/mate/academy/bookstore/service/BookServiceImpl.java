73c4ea362492c3ac02f1a45db5559c5ccdf652b3
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-12-12 10:46:24 +0200

    done

--- src/main/java/mate/academy/bookstore/service/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/BookServiceImpl.java
@@ -1,10 +1,11 @@
 package mate.academy.bookstore.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
-import mate.academy.bookstore.exception.EntityNotFoundExceptions;
+import mate.academy.bookstore.exception.EntityNotFoundException;
 import mate.academy.bookstore.mapper.BookMapper;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
@@ -33,9 +34,33 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookDto findById(Long id) {
         Book book = bookRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundExceptions("Can't find book by id " + id)
+                () -> new EntityNotFoundException("Can't find book by id " + id)
         );
 
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto requestDto) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find book by id " + id));
+
+        book.setTitle(requestDto.getTitle());
+        book.setAuthor(requestDto.getAuthor());
+        book.setIsbn(requestDto.getIsbn());
+        book.setPrice(BigDecimal.valueOf(requestDto.getPrice()));
+        book.setDescription(requestDto.getDescription());
+        book.setCoverImage(requestDto.getCoverImage());
+
+        Book saved = bookRepository.save(book);
+        return bookMapper.toDto(saved);
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new EntityNotFoundException("Can't find book by id: " + id);
+        }
+        bookRepository.deleteById(id);
     }
 }
