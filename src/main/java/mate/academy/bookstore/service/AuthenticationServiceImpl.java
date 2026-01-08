e1297a1f5cc1ea5051f0f1c378427b4edd9439d0
DmytroNikolenko <dmitriyn3679@gmail.com>
2026-01-08 10:56:10 +0200

    security

--- src/main/java/mate/academy/bookstore/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/AuthenticationServiceImpl.java
@@ -5,7 +5,10 @@ import mate.academy.bookstore.dto.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
 import mate.academy.bookstore.mapper.UserMapper;
+import mate.academy.bookstore.model.Role;
+import mate.academy.bookstore.model.RoleName;
 import mate.academy.bookstore.model.User;
+import mate.academy.bookstore.repository.RoleRepository;
 import mate.academy.bookstore.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto request)
@@ -28,6 +32,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         User user = userMapper.toModel(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RegistrationException("Role USER not found"));
+
+        user.getRoles().add(userRole);
 
         return userMapper.toDto(userRepository.save(user));
     }
